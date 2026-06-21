e6d0ff21e4e7ef8d571d66867760707ce9a14a46
Akaki <akakiburjanadze17@gmail.com>
2026-06-21 18:00:16 +0400

    Delete src/CardColor.java

--- src/CardColor.java
+++
@@ -1,9 +0,0 @@
-/**
- * The four card colors in UNO, plus {@link #NONE} for wilds and for
- * {@link GameState#calledColor} when no wild is currently in effect.
- */
-public enum CardColor {
-    R, Y, G, B,
-    /** No color: returned by wild cards and the initial/reset value of calledColor. */
-    NONE;
-}
